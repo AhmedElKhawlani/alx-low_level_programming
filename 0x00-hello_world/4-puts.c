a5078f8a93360bb907fbbd578fad6cd2f09b9df5
AhmedElKhawlani <elkhawlani.python@gmail.com>
2023-06-15 07:40:36 -0700

    Hello World 3,4,5 and 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/*
+ * 4-puts.c
+ * EL KHAWLANI Ahmed - ALX Student - elkhawlani.python@gmail.com
+ */
+
+#include <stdio>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+ * 5-printf.c
+ * EL KHAWLANI Ahmed - ALX Student - elkhawlani.python@gmail.com
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
