5ca9dfd9f7cc7d987c0e2d6f39e24e85877523a6
AhmedElKhawlani <94083919+AhmedElKhawlani@users.noreply.github.com>
2023-06-20 15:55:31 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,22 +2,10 @@
 
 /**
  * _isalpha - checks for alphabetic character
- *
- * @c: is c an integer argument
- *
- * Returns 1 if c is a letter, lowercase or uppercase, Returns 0 otherwise
+ *  c: the character to be checked
+ * Returns : 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
