035f504549c51759090d1b285d41e871256f5d0c
AhmedElKhawlani <94083919+AhmedElKhawlani@users.noreply.github.com>
2023-06-20 16:07:14 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,20 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int i, k;
 
 	i = 0;
 
 	while (i < 24)
 	{
-		j = 0;
-		while (j < 60)
+		k = 0;
+		while (k < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((k / 10) + '0');
+			_putchar((k % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
