1260b7a8d7e5efc9cab1399f37406d60cb6def39
AhmedElKhawlani <94083919+AhmedElKhawlani@users.noreply.github.com>
2023-06-20 15:55:04 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,10 @@
 
 /**
  * _islower - checks for lowercase character
- * c: letter being tested
- * Returns 1 if c is lowercase Returns 0 otherwise
+ * @c: the character to check
+ * Return: 1 if c is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
-
-	if (c >= 97 && c <= 122)
-		return (1);
-	else
-		return (0);
+	return (c >= 'a' && c <= 'z');
 }
