858da0ec52beea53ca2ddff6189429ae432392f7
AhmedElKhawlani <94083919+AhmedElKhawlani@users.noreply.github.com>
2023-06-26 15:21:36 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,13 +4,13 @@
  * print_rev - prints a string in reverse, followed by a new line
  * @s: string to be printed
  */
-void print_rev(char *s)
+void print_rev(char *ch)
 {
 	int i, j, len;
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (ch[i] != '\0')
 	{
 		i++;
 	}
@@ -19,7 +19,7 @@ void print_rev(char *s)
 
 	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[j]);
+		_putchar(ch[j]);
 	}
 
 	_putchar('\n');
