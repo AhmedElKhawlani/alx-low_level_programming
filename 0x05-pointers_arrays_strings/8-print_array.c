a1c2afec72185ba987895c4479e82330b3d01336
AhmedElKhawlani <94083919+AhmedElKhawlani@users.noreply.github.com>
2023-06-26 15:26:32 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,19 +4,19 @@
 /**
  * print_array - prints n elements of an array of integers
  * followed by a new line
- * @a: array to be printed
+ * @l: array to be printed
  * @n: number of elements to print
  */
-void print_array(int *a, int n)
+void print_array(int *l, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d", a[i]);
+			printf("%d", l[i]);
 		else
-			printf(", %d", a[i]);
+			printf(", %d", l[i]);
 	}
 		printf("\n");
 }
