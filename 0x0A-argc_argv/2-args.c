cc3e03e9adbc1c814ed6378618efa92832a43b59
AhmedElKhawlani <elkhawlani.python@gmail.com>
2023-07-07 07:41:18 -0700

    Commiting, 0x0A - 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the names of all the passed arguments
+ * @argc: Number of arguments
+ * @argv: Vector of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0)
