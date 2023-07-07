c8c3ac40a32d9512d8093aeb44d833dbb3872332
AhmedElKhawlani <elkhawlani.python@gmail.com>
2023-07-07 02:26:54 -0700

    Commiting 0x0A - 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+/**
+ * Author : EL KHAWLANI Ahmed - ALX Student
+ * Email : elkhawlani.python@gmail.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main : prints the name of the executable
+ * followed by a new line
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
