5d4fea8ca1e4d7c42db79bc6987aed1ed60a253b
AhmedElKhawlani <elkhawlani.python@gmail.com>
2023-08-01 10:16:11 -0700

    Debug 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new_list);
 	}
 
-	while (temp->next)
+	while ((*temp_node).next)
 		temp_node = (*temp_node).next;
 
 	(*temp_node).next = new_list;
