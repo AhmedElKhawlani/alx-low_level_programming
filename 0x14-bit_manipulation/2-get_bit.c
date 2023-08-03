197d3a5d640bd49fafaff9981ff8420789397c89
AhmedElkh <elkhawlani.python@gmail.com>
2023-08-03 22:36:11 +0100

    Adding 3,4 and 5

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,22 +4,23 @@
  * get_bit - returns the value of a bit at a given index
  * @n: Integer to explore
  * @index: position of bit to return
- * Return: the value of the bit at index index or -1 if an error occured
+ * Return: the value of the bit at index or -1 if an error occured
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i;
+	unsigned int j;
 
 	if (n == 0 && index < 64)
 		return (0);
-	i = 0;
-	while (i <= 63)
+
+	for (j = 0; j <= 63; n >>= 1, j++)
 	{
-		if (index == i)
+		if (index == j)
+		{
 			return (n & 1);
-		n >>= 1;
-		i++;
+		}
 	}
+
 	return (-1);
 }
