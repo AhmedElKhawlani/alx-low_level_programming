e36a8e636fa84300d3d99c49d6235d40d89ab2a7
AhmedElkh <elkhawlani.python@gmail.com>
2023-08-07 13:48:19 +0100

    Commiting 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,8 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, num_wrbytes, len = 0;
+	int file_descriptor, num_wrbytes;
+	int len = 0;
 
 	if (!filename)
 		return (-1);
