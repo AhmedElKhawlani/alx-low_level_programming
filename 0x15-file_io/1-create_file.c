3d6f6a646e5a1e935b85801ac7bd7417419d8fed
AhmedElkh <elkhawlani.python@gmail.com>
2023-08-07 13:23:06 +0100

    Debug 7

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int file_descriptor;
 	int length;
 	int num_wrbytes;
-	
+
 	file_descriptor = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 	{
