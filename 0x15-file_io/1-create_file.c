6985e37bd54f351ca6abcddcd0435b21794431d0
AhmedElkh <elkhawlani.python@gmail.com>
2023-08-07 13:31:36 +0100

    Debug 12

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		for (length = 0; text_content[length] != '\0';)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 	num_wrbytes = write(file_descriptor, text_content, length);
