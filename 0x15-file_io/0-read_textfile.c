1ddc3121f07fd7d2da522c0e9a741896e21fe5a7
AhmedElKhawlani <elkhawlani.python@gmail.com>
2023-11-27 10:25:09 +0200

    My commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile -  function that reads a text file and prints
+ * it to the POSIX standard output
+ * @filename: string representing the path to the file to read
+ * @letters: number of letters the function should read and print
+ * Return: The actual number of letters it could read and print if success
+ * 0 if fail
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_descriptor;
+	char *buffer;
+	int num_rdbytes;
+	int num_wrbytes;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!filename || !buffer)
+	{
+		free(buffer);
+		return (0);
+	}
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	num_rdbytes = read(file_descriptor, buffer, letters);
+	if (num_rdbytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	num_wrbytes = write(STDOUT_FILENO, buffer, num_rdbytes);
+	if (num_wrbytes == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(file_descriptor);
+	return (num_wrbytes);
+}
---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: string representing the path to the file to read
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor, num_wrbytes;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	num_wrbytes = write(file_descriptor, text_content, len);
+	if (file_descriptor == -1 || num_wrbytes == -1)
+		return (-1);
+	close(file_descriptor);
+	return (1);
+}
