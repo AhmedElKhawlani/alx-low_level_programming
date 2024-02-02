1dbc1880503e85db505d128ace0371b83d33c33d
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-02-02 12:54:40 +0100

    Adding first task

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
-#ifndef HASH_H
-#define HASH_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,4 +41,4 @@ void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
 
-#endif /* HASH_H*/
+#endif /* HASH_TABLES_H*/
